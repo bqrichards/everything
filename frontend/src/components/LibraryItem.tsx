2cd8f2527afb11744352523b4e411e529b005aa3
Brandon Richards <bqrichards@icloud.com>
2021-04-17 23:46:49 -0500

    Fetch thumbnails on Library page

--- frontend/src/components/LibraryItem.tsx
+++ frontend/src/components/LibraryItem.tsx
@@ -8,6 +8,9 @@ interface LibraryItemProps {
 
 export const LibraryItem: FC<LibraryItemProps> = props => (
 	<div className='LibraryItem grid-item'>
-		
+		<img
+			className='LibraryItemImage'
+			src={`api/thumbnail/${props.media.id}`}
+			alt={props.media.title} />
 	</div>
 )
